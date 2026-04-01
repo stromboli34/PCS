b67efa0b6ddfdf2ca3b0e3000c205de9f9ac7eea
stromboli34 <giulioemprin@gmail.com>
2026-04-01 11:56:23 +0000

    Terza esercitazione

---
+++ Esercitazione3/build/CMakeFiles/razionali.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/razionali.dir/link.d"
+  "CMakeFiles/razionali.dir/main.cpp.o"
+  "CMakeFiles/razionali.dir/main.cpp.o.d"
+  "razionali"
+  "razionali.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/razionali.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
