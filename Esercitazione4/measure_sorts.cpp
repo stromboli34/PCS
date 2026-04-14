ea0589780aad48d2aeda92d56d77b1705516e215
stromboli34 <giulioemprin@gmail.com>
2026-04-14 17:45:53 +0000

    Quarta esercitazione

---
+++ Esercitazione4/measure_sorts.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include "timecounter.h"
+#include "bubble_sort.h"
+#include "insert_sort.h"
+#include "select_sort.h"
+#include "randfiller.h"
+#include <cmath>
+
+ std::vector<int> genera_vettore(int lunghezza){
+    randfiller rf;
+        std::random_device rd; 
+
+        static std::mt19937 gen(std::random_device{}()); 
+    
+        
+        std::vector<int> vec(lunghezza);
+        rf.fill(vec,-22322124,5343412);
+        return(vec);
+        
+}
+
+
+int main(){
+    timecounter t;
+
+    for (int k=2;k<=13;k++){
+    std::vector<int> vec= genera_vettore(pow(2,k));
+    std::vector<int> vec_1=vec;
+    std::vector<int> vec_2=vec;
+    std::vector<int> vec_3=vec;
+
+    t.tic();
+    bubble_sort(vec_1);
+    double elapsed = t.toc();
+    std::cout << "Lunghezza: " << k << " Bubble: " << elapsed << "\n";
+    t.tic();
+    insertion_sort(vec_2);
+    elapsed = t.toc();
+    std::cout << "Lunghezza: " << k << " Insertion: " << elapsed << "\n";
+    t.tic();
+    selection_sort(vec_3);
+    elapsed = t.toc();
+    std::cout << "Lunghezza: " << k << " Selection: " << elapsed << "\n";
+    t.tic();
+    std::sort(vec.begin(),vec.end());
+    elapsed = t.toc();
+    std::cout << "Lunghezza: " << k << " Sort Nativo: " << elapsed << "\n";
+    
+    
+
+
+
+
+
+    }
+
+   
+return 0;
+
+}
